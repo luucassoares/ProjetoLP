3793af7e97883facad5e518df516dbe3d82a79d8
luucassoares <luucassoares94@gmail.com>
2015-10-02 12:09:20 -0300

    JPA COM RMI FUNCIONANDO

--- SteamProj/SteamProj-ejb/src/java/com/br/lp3/model/rmi/GenericDao.java
+++ SteamProj/SteamProj-ejb/src/java/com/br/lp3/model/rmi/GenericDao.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.br.lp3.steam;
+package steamyserver.model;
+
+import java.util.List;
 
 /**
  *
  * @author felipemramos
  */
-public class ClasseTeste {
+public interface GenericDao<T> {
+    
+    public void create(T t);
+    public List<T>  read();
+    public void update(T t);
+    public void delete(T t);
     
 }
